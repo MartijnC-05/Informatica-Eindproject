079cfcef6200630ae108212e500c2f0a78af103b
Martijn <110739@st-maartenscollege.nl>
2023-04-10 16:13:42 +0200

    Oxygen bar working!!!

---
+++ Assets/Scripts/OxygenController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Tilemaps;
+
+public class OxygenController : MonoBehaviour
+{
+    [SerializeField] private Slider oxygenSlider;
+    [SerializeField] private Tilemap waterTilemap;
+    [SerializeField] private float maxOxygen = 100f;
+    [SerializeField] private float oxygenDecreaseRate = 8f;
+    [SerializeField] private float oxygenIncreaseRate = 25f;
+    [SerializeField] private Text oxygenPercentageText;
+
+    private float currentOxygen;
+
+    private void Start()
+    {
+        currentOxygen = maxOxygen;
+        oxygenSlider.maxValue = maxOxygen;
+    }
+
+    private void Update()
+    {
+        if (IsUnderwater())
+        {
+            currentOxygen -= oxygenDecreaseRate * Time.deltaTime;
+        }
+        else
+        {
+            currentOxygen += oxygenIncreaseRate * Time.deltaTime;
+        }
+
+        currentOxygen = Mathf.Clamp(currentOxygen, 0f, maxOxygen);
+
+        oxygenSlider.value = currentOxygen;
+
+        // Update the oxygen percentage text
+        float oxygenPercentage = currentOxygen / maxOxygen * 100f;
+        oxygenPercentageText.text = oxygenPercentage.ToString("F0") + "%";
+    }
+
+    private bool IsUnderwater()
+    {
+        // Get the position of the player
+        Vector3Int position = waterTilemap.WorldToCell(transform.position);
+
+        // Check if there is a tile at the player's position
+        TileBase tile = waterTilemap.GetTile(position);
+        bool isUnderwater = tile != null;
+        return isUnderwater;
+    }
+}
